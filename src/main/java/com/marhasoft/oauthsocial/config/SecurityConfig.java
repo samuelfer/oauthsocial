052892cb4c4c0519ed313ae0f06069564f00e4ee
samuelfer <samuelfesant@gmail.com>
2023-01-10 23:46:19 -0300

    feat: realiza o login com oauth com Google e GitHub

---
+++ src/main/java/com/marhasoft/oauthsocial/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.marhasoft.oauthsocial.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        http.authorizeRequests(request ->
+            request
+                    .anyRequest().authenticated()).oauth2Login();
+        return http.build();
+
+    }
+}
---
+++ src/test/java/com/marhasoft/oauthsocial/OauthsocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marhasoft.oauthsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthsocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
