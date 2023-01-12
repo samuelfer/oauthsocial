f3ff0226c6b0c41827c15c86e53c38c5e7e31f2b
samuelfer <samuelfesant@gmail.com>
2023-01-11 22:27:03 -0300

    feat: remove config de clients do arquivo yml

--- src/main/java/com/marhasoft/oauthsocial/controller/SocialLoginOauth2Controller.java
+++ src/main/java/com/marhasoft/oauthsocial/controller/SocialLoginOauth2Controller.java
@@ -1,26 +1,19 @@
 package com.marhasoft.oauthsocial.controller;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Map;
 
 @Controller
 @RequestMapping
 public class SocialLoginOauth2Controller {
 
     @GetMapping("/")
-    public String getIndex(Model model, Authentication authentication) {
-        DefaultOAuth2User user = (DefaultOAuth2User) authentication.getPrincipal();
-        Map<String, Object> attributes = user.getAttributes();
-
-        String name = attributes.getOrDefault("localizedFirstName", attributes.get("name")).toString();
-
-        model.addAttribute("name", name);
+    public String getIndex(Model model, @AuthenticationPrincipal OAuth2User principal) {
+        model.addAttribute("name", principal.getAttribute("name"));
         return "index";
     }
 
